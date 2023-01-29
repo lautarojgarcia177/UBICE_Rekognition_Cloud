e74a6dc4e98d6e358f13e506abaa96605eb3e70a
Lautaro Garcia <lautarojgarcia177@gmail.com>
2023-01-29 13:20:09 -0300

    Inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useToast } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { Outlet } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+function startUp() {
+  // TODO Verificar que esten las credenciales cargadas
 }
 
-export default App
+export default function App() {
+  const toast = useToast();
+  useEffect(() => startUp(), []);
+  return <Outlet />;
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./error-page";
+import { ChakraProvider } from '@chakra-ui/react'
+import UploadPhotos from "./routes/UploadPhotos";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+export const appRoutes = {
+  root: "/",
+  uploadPhotos: "uploadPhotos"
+};
+
+const router = createBrowserRouter([
+  {
+    path: appRoutes.root,
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: appRoutes.uploadPhotos,
+        element: <UploadPhotos />
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider>
+      <RouterProvider router={router} />
+    </ChakraProvider>
+  </React.StrictMode>
+);
